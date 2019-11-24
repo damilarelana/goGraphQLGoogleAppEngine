a20d90ea47264eb661682a30dc611d1586598f1c
Damilare Lana <damilarelana@gmail.com>
2019-11-24 02:06:13 +0100

    clean up unexpected newline

--- middleware.go
+++ middleware.go
@@ -2,24 +2,18 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
-
-	"github.com/pkg/errors"
 )
 
 // responseError endpoint handler
 func responseError(w http.ResponseWriter, errMsg string, errCode int) {
 	w.Header().Set("Content-Type", "application/json: charset=UFT-8") // set the content header type
 	w.WriteHeader(errCode)
-	json.NewEncoder(w).Encode(map[string]string{
-		"error": errMsg
-	})
+	json.NewEncoder(w).Encode(map[string]string{"error": errMsg})
 }
 
 // responseJSON endpoint handler
-func responseJSON(w http.ResponseWriter, data  interface{}) {
+func responseJSON(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "application/json: charset=UFT-8") // set the content header type
 	json.NewEncoder(w).Encode(data)
 }
