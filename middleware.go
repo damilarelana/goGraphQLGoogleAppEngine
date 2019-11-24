4f6101e9c7fbe09f3b745383e7cf44237c29473c
Damilare Lana <damilarelana@gmail.com>
2019-11-24 02:04:53 +0100

    complete initial setup

---
+++ middleware.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+// responseError endpoint handler
+func responseError(w http.ResponseWriter, errMsg string, errCode int) {
+	w.Header().Set("Content-Type", "application/json: charset=UFT-8") // set the content header type
+	w.WriteHeader(errCode)
+	json.NewEncoder(w).Encode(map[string]string{
+		"error": errMsg
+	})
+}
+
+// responseJSON endpoint handler
+func responseJSON(w http.ResponseWriter, data  interface{}) {
+	w.Header().Set("Content-Type", "application/json: charset=UFT-8") // set the content header type
+	json.NewEncoder(w).Encode(data)
+}
---
+++ model.go
@@ -0,0 +1,16 @@
+package main
+
+import "time"
+
+type User struct (
+	ID string `json:"id" datastore:"-"`
+	Name string `json:"name"`
+)
+
+
+type Post struct{
+	ID string `json:"id" datastore:"-"`
+	UserID string `json:"userId"`
+	CreatedAt time.Time `json:"createdAt"`
+	Content string `json:"content"`
+}
