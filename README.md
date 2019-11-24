ddc930dad95f8d453ed4289b158900b03eb84b31
Damilare Lana <damilarelana@gmail.com>
2019-11-24 02:31:51 +0100

    cleaned up the comments in model.go

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 A golang implementation of GraphQL server, via the Google App Engine server-less platform
 
 ***
+
+* Golang
+* GraphQL
+* Google App Engine
--- model.go
+++ model.go
@@ -2,15 +2,16 @@ package main
 
 import "time"
 
-type User struct (
-	ID string `json:"id" datastore:"-"`
+// User fields declared
+type User struct {
+	ID   string `json:"id" datastore:"-"`
 	Name string `json:"name"`
-)
+}
 
-
-type Post struct{
-	ID string `json:"id" datastore:"-"`
-	UserID string `json:"userId"`
+// Post fields declared
+type Post struct {
+	ID        string    `json:"id" datastore:"-"`
+	UserID    string    `json:"userId"`
 	CreatedAt time.Time `json:"createdAt"`
-	Content string `json:"content"`
+	Content   string    `json:"content"`
 }
