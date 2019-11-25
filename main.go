d4177dba07526377ab0ed62cbe19ccabf630c724
Damilare Lana <damilarelana@gmail.com>
2019-11-25 07:03:01 +0100

    renamed 'PostList' to 'userTypePostList' and 'rootFieldsPostList' under variables 'userType' and 'rootFields' respectively

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ var userType = graphql.NewObject(graphql.ObjectConfig{ // declare GraphQL userTy
 	Fields: graphql.Fields{
 		"id":    &graphql.Field{Type: graphql.String},
 		"name":  &graphql.Field{Type: graphql.String},
-		"posts": makeListField(makeNodeListType("userPostList", postType), resolvers.QueryPostsByUser),
+		"posts": makeListField(makeNodeListType("userTypePostList", postType), resolvers.QueryPostsByUser),
 	},
 })
 
@@ -109,7 +109,7 @@ var rootFields = graphql.Fields{ // declare query fields.
 	},
 
 	// queryPost field
-	"posts": makeListField(makeNodeListType("queryPostList", postType), resolvers.QueryPosts),
+	"posts": makeListField(makeNodeListType("rootFieldPostList", postType), resolvers.QueryPosts),
 }
 
 var rootQuery = graphql.NewObject(graphql.ObjectConfig{ // declare rootQuery
